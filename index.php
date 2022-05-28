cab936b2201424d49df0d3cb525302b1b37227e7
Bill <billvanleeuwen424@gmail.com>
2022-05-28 18:45:54 +0000

    added html boilerplate

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 
 ?>
-<html>
-<span>First PAge of the hackathon!!!!!!!!</span>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hackothon</title>
+    <link rel="stylesheet" href="https://unpkg.com/purecss@2.1.0/build/pure-min.css" integrity="sha384-yHIFVG6ClnONEA5yB5DJXfW2/KC173DIQrYoZMEtBvGzmf0PKiGyNEqe9N6BNDBH" crossorigin="anonymous">
+</head>
+<body>
+    
+</body>
 </html>
