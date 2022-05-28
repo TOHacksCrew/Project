a0119992f9df33d0d665f857f2015f3773a317b5
Avery Chin <avery.chin@gmail.com>
2022-05-28 21:56:10 +0000

    shfiujhgkdfg

--- index.php
+++ index.php
@@ -1,57 +1,56 @@
-<?php
-
-?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hackathon</title>
-    <link rel="stylesheet" href="stlyes/master.css">
-    <link rel="stylesheet" href="https://unpkg.com/purecss@2.1.0/build/pure-min.css" integrity="sha384-yHIFVG6ClnONEA5yB5DJXfW2/KC173DIQrYoZMEtBvGzmf0PKiGyNEqe9N6BNDBH" crossorigin="anonymous">
+    <meta name="description" content="A layout example that shows off a responsive product landing page.">
+    <title>Landing Page &ndash; Layout Examples &ndash; Pure</title>
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+    <link rel="stylesheet" href="styles/master.css">
 </head>
 <body>
-    <header>
-        <h1>
-            Title of Project
-        </h1>
-    </header>
-    <main>
-        <form action="/action_page.php">
-       
-     
-        <label for="firstname">Enter your first name:</label>
-        <input type="firstname" id="firstname" name="firstname">
-            <br>
-            <label for="lastname">Enter your last name:</label>
-        <input type="lastname" id="lastname" name="lastname">
-            <br>
-            <label for="email">Enter your email:</label>
-        <input type="email" id="email" name="email">
-        <br>
-        <input type="submit">
-            
 
-        </form>
+<div class="splash-container">
+    <div class="splash">
+        <h1 class="splash-head">Big Bold Text</h1>
+        <p class="splash-subhead">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        </p>
+        <p>
+            <a href="http://purecss.io" class="pure-button pure-button-primary">Get Started</a>
+        </p>
+    </div>
+</div>
+
+<div class="content-wrapper">
+    <div class="content">
+
+    <div class="content">
+        <h2 class="content-head is-center">Dolore magna aliqua. Uis aute irure.</h2>
+
         <div class="pure-g">
-            <div class="pure-u-1-2">
-                <span>Description</span>
+            <div class="l-box-lrg pure-u-1 pure-u-md-2-5">
+                <form class="pure-form pure-form-stacked">
+                    <fieldset>
+
+                        <label for="name">Your Name</label>
+                        <input id="name" type="text" placeholder="Your Name">
+
+
+                        <label for="email">Your Email</label>
+                        <input id="email" type="email" placeholder="Your Email">
+
+                        <label for="password">Your Password</label>
+                        <input id="password" type="password" placeholder="Your Password">
+
+                        <button type="submit" class="pure-button">Sign Up</button>
+                    </fieldset>
+                </form>
             </div>
         </div>
-        <div class="pure-g">
-            <div class="pure-u-1-2">
-                <span>Filter</span>
-            </div>
-        </div>
-        <div class="pure-g">
-            <div class="pure-u-1-2">
-                <span>Map</span>
-            </div>
-        </div>
-    </main>
-    <footer>
-        <span>TOHacks 2022</span>
-    </footer>
+
+    </div>
+</div>
+
 </body>
 </html>
