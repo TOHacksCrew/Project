141d4d0ed590bc10057e716caf639310daeae10f
Bill <billvanleeuwen424@gmail.com>
2022-05-28 19:06:29 +0000

    embedded google maps

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
     <link rel="stylesheet" href="https://unpkg.com/purecss@2.1.0/build/pure-min.css" integrity="sha384-yHIFVG6ClnONEA5yB5DJXfW2/KC173DIQrYoZMEtBvGzmf0PKiGyNEqe9N6BNDBH" crossorigin="anonymous">
 </head>
 <body>
-    
+    <div class="googleMap">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d81382.31549935584!2d-78.34904935233745!3d44.317301941907786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sca!4v1629498728316!5m2!1sen!2sca" style="border:0;" frameborder=0></iframe>
+    </div>
 </body>
 </html>
