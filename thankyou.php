69e54afda40fc6eada8c19574484af90b0576cba
Avery Chin <avery.chin@gmail.com>
2022-05-29 13:44:03 +0000

    title

--- thankyou.php
+++ thankyou.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Arbor</title>
+    <title>Tree Huggers</title>
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
     <link rel="stylesheet" href="styles/master.css">
 </head>
