4e67e6edbdb36b61dad43205aa15e1156280154d
Avery Chin <avery.chin@gmail.com>
2022-05-29 03:35:24 +0000

    changes to thankyou.php

--- thankyou.php
+++ thankyou.php
@@ -1,1 +1,30 @@
-<!---test>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arbor</title>
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+    <link rel="stylesheet" href="styles/master.css">
+</head>
+<body>
+
+<div class="splash-container">
+    <div class="splash">
+        <h1 class="splash-head">Tree Hugger</h1>
+        <p class="splash-subhead">
+            Description of our site
+        </p>
+    </div>
+</div>
+
+<div class="content-wrapper">
+    <div class="content">
+        <h2 id="start" class="content-head is-center">Thank You For Your Contribution &#128512</h2>
+    </div>
+</div>
+
+</body>
+</html>
